35cc036b402a0f85c2593f92c58800d51da4a3eb
roycan <roy.canseco@gmail.com>
2020-05-16 05:00:24 +0800

    display officials based on zipcode from google civicinfo_v2 api

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,6 @@
 require "csv"
+require 'google/apis/civicinfo_v2'
+
 
 def clean_zipcode(zipcode)
 
@@ -22,6 +24,13 @@ contents.each do |row|
 
   zipcode = clean_zipcode(zipcode)
 
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+  response = civic_info.representative_info_by_address(address: 80202,
+                          levels: 'country',
+                          roles: ['legislatorUpperBody',
+                            'legislatorLowerBody'])
+  legilators = response.officials
 
-  puts "#{name}  #{zipcode}"
+  puts "#{name}  #{zipcode} #{legilators}"
 end
