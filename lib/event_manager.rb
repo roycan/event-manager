4bdd417d507bf154c426e6c0e31a3046a6e2c169
roycan <roy.canseco@gmail.com>
2020-05-15 22:46:05 +0800

    read firstnames from csv

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,9 @@
 puts "Event Manager Initialized"
+
+lines = File.readlines ("event_attendees.csv") if File.exist? ("event_attendees.csv")
+# Dir.glob("*").each { |f| puts f}
+
+lines.each_with_index do |line, idx|
+  next if idx == 0
+  puts line.split(",")[2]
+end
