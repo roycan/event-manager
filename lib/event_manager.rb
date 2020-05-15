e018ade30826cea4e47d667e9972503af30899a9
roycan <roy.canseco@gmail.com>
2020-05-16 04:33:12 +0800

    format zipcode as 5 digits

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -8,5 +8,13 @@ contents = CSV.open("event_attendees.csv", headers: true, header_converters: :sy
 contents.each do |row|
   name = row[:first_name]
   zipcode = row[:zipcode]
+
+  # zip codes are 5 digit numbers
+  # pad zeros in front to make 5 digits
+  # truncate when necessary
+
+  zipcode = zipcode.to_s.rjust(5, "0")[0..4]
+
+
   puts "#{name}  #{zipcode}"
 end
