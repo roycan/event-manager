ac8a92151da52a706cbaf0884af80ad4cbdd42d3
roycan <roy.canseco@gmail.com>
2020-05-16 05:46:16 +0800

    create thank you letters to participants

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -35,6 +35,8 @@ puts "Event Manager Initialized"
 contents = CSV.open("event_attendees.csv", headers: true, header_converters: :symbol) if File.exist? ("event_attendees.csv")
 # Dir.glob("*").each { |f| puts f}
 
+template_letter = File.read("form_letter.html")
+
 contents.each do |row|
   name = row[:first_name]
   zipcode = row[:zipcode]
@@ -44,4 +46,10 @@ contents.each do |row|
   legislators = legislators_by_zipcode(zipcode)
 
   puts "#{name}  #{zipcode} #{legislators}"
+
+  personal_letter = template_letter.gsub("FIRST_NAME", name)
+  personal_letter = personal_letter.gsub("LEGISLATORS", legislators)
+
+  puts personal_letter
+
 end
